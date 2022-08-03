3e38a5a7803364895b01e6ffbcb4e0bbdb5eca72
vattghern <otavioaugustobrb19@gmail.com>
2022-08-03 20:46:20 -0300

    aula 02

---
+++ aula002.py
@@ -0,0 +1,30 @@
+# Metacaracteres: ^ $ * ? { } [ ] \ | ( )
+
+# | = OU.
+# . = Qualquer caractere (com exceçãp da quebra de linha).
+# [] = Conjunto de caracteres.
+
+
+import re
+
+texto = '''
+João trouxe    flores para sua amada namorada em 10 de janeiro de 1970,
+Maria era o nome dela.
+Foi um ano excelente na vida de joão. Teve 5 filhos, todos adultos atualmente.
+maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de
+domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso
+pão de queijo.
+Não canso de ouvir a Maria:
+"Joooooooooãooooooo, o café tá prontinho aqui. Veeemm"!
+'''
+
+print(re.findall(r'João|Maria|ad....s', texto))
+
+print(re.findall(r'João|Maria|ad....s', texto))
+print(re.findall(r'[Jj]oão|[Mm]aria', texto))
+
+# Range de A a Z tanto maiúsculo como minúsculo. Como também de 0 a 9
+print(re.findall(r'[a-zA-Z0-9_.]aria|[a-zA-Z0-9]oão', texto))
+
+# Ignora o case sensitive, ou seja, não diferencia maiúscula de minúscula.
+print(re.findall(r'MAriA|jOãO', texto, flags=re.IGNORECASE))
